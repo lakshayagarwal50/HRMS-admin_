df61648fc2857893507fa1727a3be3542bd44eaf
Lakshay Agarwal <lakshay.agarwal1@appinventiv.com>
2025-09-05 11:12:05 +0530

    css changes

--- src/features/Attendance/pages/AttendanceSummary.tsx
+++ src/features/Attendance/pages/AttendanceSummary.tsx
@@ -385,7 +385,7 @@ const AttendanceSummary = () => {
   };
 
   return (
-    <div className="p-4 sm:p-6 lg:p-8 w-[97rem]">
+    <div className="p-4 sm:p-6 lg:p-8">
       {view === "list" ? (
         <AttendanceListView
           employees={employees}
